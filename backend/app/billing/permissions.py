1ef584e8161a69d94073b19bbfd566305c695bf7
gadbahati <124551248+gadbahati@users.noreply.github.com>
2026-09-10 19:07:12 +0300

    feat: define billing permissions

---
+++ backend/app/billing/permissions.py
@@ -0,0 +1,4 @@
+BILLING_SERVICE_WRITE = "billing.service.write"
+BILLING_CHARGE_WRITE = "billing.charge.write"
+BILLING_INVOICE_WRITE = "billing.invoice.write"
+BILLING_PAYMENT_WRITE = "billing.payment.write"
