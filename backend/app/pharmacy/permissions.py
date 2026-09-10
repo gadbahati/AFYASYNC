acb17bab0b0cc349b88970fd1f9f71904ff459c5
gadbahati <124551248+gadbahati@users.noreply.github.com>
2026-09-10 19:00:05 +0300

    feat: define pharmacy permissions

---
+++ backend/app/pharmacy/permissions.py
@@ -0,0 +1,4 @@
+PHARMACY_CREATE_MEDICATION = "pharmacy.medication.create"
+PHARMACY_CREATE_PRESCRIPTION = "pharmacy.prescription.create"
+PHARMACY_RECEIVE_INVENTORY = "pharmacy.inventory.receive"
+PHARMACY_DISPENSE = "pharmacy.prescription.dispense"
